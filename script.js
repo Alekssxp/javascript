4da11ead7e3227f3ca1a937181e8d8538212bd32
Alekss <alekss_xp@mail.ru>
2020-10-16 02:43:55 +0300

    task3functionRedaction

--- script.js
+++ script.js
@@ -58,6 +58,8 @@ function showMyDB (hidden) {
     }
 }
 
+showMyDB();
+
 function writeYourGenres() {
     for (let i = 1; i <= 3; i++) {
         personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
